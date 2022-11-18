772bd96a502cf09fb5c1270c2187f82a3c4e72a0
pokhilenkod <pokhilenko.d@gmail.com>
2022-11-18 11:41:46 +0100

    adds resolved first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+  btnStartEl: document.querySelector('[data-start]'),
+  btnStopEl: document.querySelector('[data-stop]'),
+  bodyEl: document.querySelector('body'),
+  randomColorInterval: null,
+
+};
+
+refs.btnStartEl.addEventListener('click', onStartRandomColor);
+refs.btnStopEl.addEventListener('click', onStopRandomColor);
+
+function onStartRandomColor(event) {
+   refs.randomColorInterval = setInterval(() => {refs.bodyEl.style.backgroundColor = getRandomHexColor()}, 1000);
+   refs.btnStartEl.disabled = 'true';
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+function onStopRandomColor(event){
+clearInterval(refs.randomColorInterval);
+refs.btnStartEl.disabled = !'true';
+refs.bodyEl.style.backgroundColor = '#fafafa';
+
+}
 
