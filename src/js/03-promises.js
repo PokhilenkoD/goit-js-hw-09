70f41b6740dfdabdd50957f5d64972137718764d
pokhilenkod <pokhilenko.d@gmail.com>
2022-11-20 15:38:17 +0100

    adds resolved third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  return promise;
+}
+
+const refs = {
+  formEl: document.querySelector('.form'),
+};
+
+refs.formEl.addEventListener('submit', onCreatePromise);
+
+function onCreatePromise(event) {
+  event.preventDefault();
+  const delay = Number(event.target[0].value);
+  const step = Number(event.target[1].value);
+  const amount = Number(event.target[2].value);
+
+  for (let i = 0; i < amount; i += 1) {
+    createPromise(i + 1, delay + i * step)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
